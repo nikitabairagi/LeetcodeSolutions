4b003f231ca626d55fbf042256613a3d9244bc10
nikitabairagi <nikitabairagi@gmail.com>
2019-12-29 17:14:24 -0800

    educative io solutions

---
+++ Other/IdenticalTree.java
@@ -0,0 +1,16 @@
+class identicalTrees{
+    public static boolean are_identical( BinaryTreeNode root1, BinaryTreeNode root2) {
+
+        if(root1 == null && root2 == null){
+            return true;
+        }
+        if(root1 != null && root2 != null){
+            if(root1.data == root2.data &&
+                    are_identical(root1.left, root2.left) &&
+                    are_identical(root1.right,root2.right)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
